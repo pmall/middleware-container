a4cccc19edb315494bf62efadcab7f4cbd9a3ec7
pmall <pierre.mallinjoud@gmail.com>
2017-12-08 19:31:13 +0100

    add integration test

--- spec/ContainerMiddleware.spec.php
+++ spec/ContainerMiddleware.spec.php
@@ -3,6 +3,7 @@
 use function Eloquent\Phony\Kahlan\mock;
 
 use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -46,6 +47,79 @@ describe('ContainerMiddleware', function () {
             $test = $this->middleware->process($request, $handler);
 
             expect($test)->toBe($response);
+
+        });
+
+    });
+
+});
+
+describe('ContainerMiddleware', function () {
+
+    beforeAll(function () {
+
+        class TestMiddleware implements MiddlewareInterface
+        {
+            private $dependency1;
+            private $dependency2;
+
+            public function __construct(TestDependency1 $dependency1, TestDependency2 $dependency2)
+            {
+                $this->dependency1 = $dependency1;
+                $this->dependency2 = $dependency2;
+            }
+
+            public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+            {
+                if ($this->dependency1 == new TestDependency1($request, $handler)) {
+
+                    return mock(ResponseInterface::class)->get();
+
+                }
+            }
+        }
+
+        class TestDependency1
+        {
+            private $request;
+            private $handler;
+
+            public function __construct(ServerRequestInterface $request, RequestHandlerInterface $handler)
+            {
+                $this->request = $request;
+                $this->handler = $handler;
+            }
+        }
+
+        class TestDependency2
+        {
+            //
+        }
+
+    });
+
+    describe('->__invoke()', function () {
+
+        it('should inject the request and request handler into the middleware dependency', function () {
+
+            $dependency2 = new TestDependency2;
+
+            $container = mock(ContainerInterface::class);
+
+            $exception = mock([Throwable::class, NotFoundExceptionInterface::class])->get();
+
+            $container->get->with(TestMiddleware::class)->throws($exception);
+            $container->get->with(TestDependency1::class)->throws($exception);
+            $container->get->with(TestDependency2::class)->returns($dependency2);
+
+            $request = mock(ServerRequestInterface::class)->get();
+            $handler = mock(RequestHandlerInterface::class)->get();
+
+            $middleware = new ContainerMiddleware($container->get(), TestMiddleware::class);
+
+            $test = $middleware->process($request, $handler);
+
+            expect($test)->toBeAnInstanceOf(ResponseInterface::class);
 
         });
 
